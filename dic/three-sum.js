e115221d57b67cbeb2deb7d7abe5f80d10d2554f
luofeng <devinxian@gmail.com>
2020-09-24 15:48:26 +0800

    三数之和map缓存版

--- dic/three-sum.js
+++ dic/three-sum.js
@@ -1,3 +1,53 @@
+function check(arr) {
+  return !!arr && arr.length > 0
+}
+
+/*** 三数之和 */
+exports.threeSumRoughly = function (arr, sum) {
+  const res = []
+  if (!check(arr)) return res
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        if (arr[i] + arr[j] + arr[k] === sum) {
+          res.push([arr[i], arr[j], arr[k]])
+        }
+      }
+    }
+  }
+
+  return res
+}
+
+exports.threeSum = function (arr, sum) {
+  const res = []
+  const map = new Map()
+
+  if (!check(arr)) return res
+
+  for (let i = 0; i < arr.length; i++) {
+    map.clear() // 必须
+
+    // 内层做 twoSum
+    const remain = sum - arr[i]
+
+    for (let j = i + 1; j < arr.length; j++) {
+      const rest = remain - arr[j]
+
+      if (map.get(rest)) {
+        res.push([arr[i], arr[j], rest])
+      } else {
+        map.set(arr[j], rest)
+      }
+    }
+  }
+
+  return res
+}
+
+
+
 /*** 两数之和 */
 
 /**
@@ -5,8 +55,7 @@
  */
 exports.twoSumRoughly = function (arr, sum) {
   const res = []
-
-  if (!arr || !arr.length) return []
+  if (!check(arr)) return res
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
@@ -26,7 +75,7 @@ exports.twoSum = function (arr, sum) {
   const res = []
   const map = new Map()
 
-  if (!arr || !arr.length) return []
+  if (!check(arr)) return res
 
   for (let i = 0; i < arr.length; i++) {
     const ele = arr[i]
--- dic/three-sum.test.js
+++ dic/three-sum.test.js
@@ -1,10 +1,38 @@
 // 给定数组，输出连续最大的乘积
-const { twoSumRoughly, twoSum } = require('./three-sum')
+const { twoSumRoughly, twoSum, threeSumRoughly, threeSum } = require('./three-sum')
+
+const list = [1, 2, 3, -1, 7, 10]
+
+describe('three sum by map', () => {
+  test('should return empty', () => {
+    expect(threeSum([], 8)).toHaveLength(0)
+    expect(threeSum(list, 100)).toHaveLength(0)
+  })
+  test('should return three records', () => {
+    expect(threeSum(list, 10)).toHaveLength(2)
+  })
+  test('should return three records', () => {
+    expect(threeSum(list, 12)).toHaveLength(2)
+  })
+})
+
+describe('three sum by loop', () => {
+  test('should return empty', () => {
+    expect(threeSumRoughly([], 8)).toHaveLength(0)
+    expect(threeSumRoughly(list, 100)).toHaveLength(0)
+  })
+  test('should return three records when sum is 10', () => {
+    expect(threeSumRoughly(list, 10)).toHaveLength(2)
+  })
+  test('should return three records when sum is 12', () => {
+    expect(threeSumRoughly(list, 12)).toHaveLength(2)
+  })
+})
 
 const arr = [2, 3, 4, -1, 7, 5, 1, 6]
 const arr2 = [2, 3, 4, -1, 7]
 
-describe('two sum loop', () => {
+describe('two sum by loop', () => {
   test('should return empty', () => {
     expect(twoSumRoughly([], 8)).toHaveLength(0)
   })
