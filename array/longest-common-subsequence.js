a33fb0ff7e3f4566b8f479c058b4a2f685a2ce5d
luofeng <devinxian@gmail.com>
2020-09-30 00:28:42 +0800

    最长公共子序列长度

--- array/longest-common-subsequence.js
+++ array/longest-common-subsequence.js
@@ -1,19 +1,36 @@
 /**
- * leetcode 1143 求最长公共子序列
- * 如 abcde 和 acek 的最长公共子序列是 ace
+ * leetcode 1143 求最长公共子序列长度
+ * 如 abcde 和 acek 的最长公共子序列是 ace, 结果为3
  */
 exports.solutionByDP = function (a, b) {
   /**
-   * 定义DP状态: dpa[i] dpb[j] 表示到 a[i] 和 b[j] 的最长公共子序列
+   * 定义DP状态: dpa[i] dpb[j] 表示到 a[i] 和 b[j] 的最长公共子序列长度
    * 确定DP转移方程:
-   * dp[i][j] = a[i] === b[j] ? dp[i-1][j-1].push(a[i]): []
-   *    不算 a[i] 时最长子序列
-   *    不算 b[j] 时最长子序列
-   * a [1, 2, 3, 6]
-   * b [2, 3, 2, 3, 6]
+   *  如果 a[i] === b[j]，则公共子序列长度 +1
+   *  否则：a[i - 1]b[j] 和 a[i]b[j - 1] 最大值
    */
-  const dpa = [a[0]]
-  const dpb = [b[0]]
+  let res = 0
+  const dp = []
+  for (let k = 0; k < a.length; k++) {
+    dp[k] = new Array(b.length)
+    dp[k].fill(0)
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < b.length; j++) {
+      if (i === 0 || j === 0) {
+        dp[i][j] = a[i] === b[j] ? 1 : 0
+      } else {
+        dp[i][j] = a[i] === b[j]
+          ? dp[i - 1][j - 1] + 1
+          : Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+
+      res = Math.max(res, dp[i][j])
+    }
+  }
+
+  return res
 }
 
 /**
@@ -21,7 +38,7 @@ exports.solutionByDP = function (a, b) {
  * // TODO: 输出公共子序列值
  */
 exports.longestContinuousSubArray = function (a, b) {
-  if (!a || !a.length || !b || !b.length) return 0 
+  if (!a || !a.length || !b || !b.length) return 0
 
   let res = 0
   const dp = []
--- array/longest-common-subsequence.test.js
+++ array/longest-common-subsequence.test.js
@@ -1,4 +1,4 @@
-const { longestContinuousSubArray } = require('./longest-common-subsequence')
+const { longestContinuousSubArray, solutionByDP } = require('./longest-common-subsequence')
 const ltsa = longestContinuousSubArray 
 
 describe('longest subarray length', () => {
@@ -13,5 +13,20 @@ describe('longest subarray length', () => {
   })
   test('common input', () => {
     expect(ltsa([1, 2, 3], [3, 2, 3, 4])).toEqual(2)
+  })
+})
+
+describe('longest common subsequence length', () => {
+  test('both empty', () => {
+    expect(solutionByDP([], [])).toEqual(0)
+  })
+  test('one empty', () => {
+    expect(solutionByDP([], [1, 2, 3])).toEqual(0)
+  })
+  test('same input', () => {
+    expect(solutionByDP([1, 2, 3], [1, 2, 3])).toEqual(3)
+  })
+  test('common input', () => {
+    expect(solutionByDP([1, 2, 3, 6, 4, 1, 6], [3, 2, 3, 4, 0, 6])).toEqual(4)
   })
 })
